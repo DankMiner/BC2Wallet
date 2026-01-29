055b56217d6f4f788e5f4b6d46a30413b57624da
DankMiner <support@1miner.net>
2026-01-28 22:32:59 -0600

    Add BC2 network support to HDSegwitBech32Wallet
    Updated the wallet class to include BC2 network support and modified the derivation path. Adjusted methods to reflect the lack of support for PayJoin, BIP47, and Silent Payments for BC2.

--- class/wallets/hd-segwit-bech32-wallet.ts
+++ class/wallets/hd-segwit-bech32-wallet.ts
@@ -1,8 +1,9 @@
 import { AbstractHDElectrumWallet } from './abstract-hd-electrum-wallet';
+import BC2_MAINNET, { BC2_COIN_TYPE } from '../../blue_modules/bc2-network';
 
 /**
  * HD Wallet (BIP39).
- * In particular, BIP84 (Bech32 Native Segwit)
+ * In particular, BIP84 (Bech32 Native Segwit) for BitcoinII (BC2)
  * @see https://github.com/bitcoin/bips/blob/master/bip-0084.mediawiki
  */
 export class HDSegwitBech32Wallet extends AbstractHDElectrumWallet {
@@ -13,7 +14,16 @@ export class HDSegwitBech32Wallet extends AbstractHDElectrumWallet {
   // @ts-ignore: override
   public readonly typeReadable = HDSegwitBech32Wallet.typeReadable;
   public readonly segwitType = 'p2wpkh';
-  static readonly derivationPath = "m/84'/0'/0'";
+
+  // BC2 derivation path: m/84'/2'/0' (coin type 2 for BC2)
+  static readonly derivationPath = `m/84'/${BC2_COIN_TYPE}'/0'`;
+
+  /**
+   * Returns the BC2 network configuration for address generation
+   */
+  getNetwork() {
+    return BC2_MAINNET;
+  }
 
   allowSend() {
     return true;
@@ -24,7 +34,7 @@ export class HDSegwitBech32Wallet extends AbstractHDElectrumWallet {
   }
 
   allowPayJoin() {
-    return true;
+    return false; // Disabled for BC2 - no PayJoin support yet
   }
 
   allowCosignPsbt() {
@@ -48,10 +58,10 @@ export class HDSegwitBech32Wallet extends AbstractHDElectrumWallet {
   }
 
   allowBIP47() {
-    return true;
+    return false; // Disabled for BC2 - no BIP47 support yet
   }
 
   allowSilentPaymentSend(): boolean {
-    return true;
+    return false; // Disabled for BC2 - no Silent Payments support yet
   }
 }
